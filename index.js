c844ea07cceba6f74e73cc65e86208787abb557d
adeyemi1915 <135009534+adeyemi1915@users.noreply.github.com>
2023-06-20 16:16:57 +0300

    complete ass

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers()
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2); // Return the first two drivers
+  }
+  
+  // returnLastTwoDrivers()
+  const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2); // Return the last two drivers
+  }
+  
+  // selectingDrivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier()
+  const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+      return fare * multiplier; // Multiply the fare by the multiplier
+    }
+  }
+  
+  // fareDoubler()
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler()
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers()
+  const selectDifferentDrivers = (drivers, driverSelector) => {
+    return driverSelector(drivers); // Call the driverSelector function with the drivers array
+  }
+  
